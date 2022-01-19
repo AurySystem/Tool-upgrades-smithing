5158c10bf00afc810ad19d360db1bfe7f40e709c
Maddy <22009843+AurySystem@users.noreply.github.com>
2022-01-19 01:21:23 -0500

    added builtin optional datapacks to overide the defualt settings

--- src/main/java/gay/Aurum/smithingupgrades/SmithingUpgrades.java
+++ src/main/java/gay/Aurum/smithingupgrades/SmithingUpgrades.java
@@ -1,6 +1,10 @@
 package gay.Aurum.smithingupgrades;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
 
 public class SmithingUpgrades implements ModInitializer {
 	public static final String MOD_ID = "smithingupgrades";
@@ -8,6 +12,11 @@ public class SmithingUpgrades implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER("Loading");
+		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+			ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID,"costpertool"),modContainer, ResourcePackActivationType.NORMAL);
+			ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID,"costpermaterial3"),modContainer, ResourcePackActivationType.NORMAL);
+			ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID,"costpermaterial4"),modContainer, ResourcePackActivationType.NORMAL);
+		});
 	}
 	public static void LOGGER(String s){
 		System.out.println(MOD_ID+": "+s);
